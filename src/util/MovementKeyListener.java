0a94896848cd21a26bffa733bb4c9aa84fa291d7
Quintin Harter <qharter@gmail.com>
2018-05-25 12:53:52 -0400

    control update

--- src/util/MovementKeyListener.java
+++ src/util/MovementKeyListener.java
@@ -3,20 +3,27 @@ package util;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.Timer;
+
 import game.Game;
 
 /**
  * The key listener for movement. Maps the W, A, S, and D keys to moving.
+ * 
  * @author Quintin Harter
  *
  */
-public class MovementKeyListener  implements KeyListener{
-	
+public class MovementKeyListener implements KeyListener {
+
 	private Game game;
-	
+	private char justPressed = '~';
+	private boolean released = true;
+
 	/**
 	 * Constructor
-	 * @param game the game to run the key listener in
+	 * 
+	 * @param game
+	 *            the game to run the key listener in
 	 */
 	public MovementKeyListener(Game game) {
 		this.game = game;
@@ -24,27 +31,29 @@ public class MovementKeyListener  implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		//move the player
-		if (e.getKeyChar() == 'w')
-			game.attemptMove(Vector.VECTOR_UP);
-		else if (e.getKeyChar() == 'd')
-			game.attemptMove(Vector.VECTOR_RIGHT);
-		else if (e.getKeyChar() == 's')
-			game.attemptMove(Vector.VECTOR_DOWN);
-		else if (e.getKeyChar() == 'a')
-			game.attemptMove(Vector.VECTOR_LEFT);
-		
+		// move the player
+		if (released || justPressed != e.getKeyChar()) {
+			released = false;
+			justPressed = e.getKeyChar();
+			if (justPressed == 'w')
+				game.attemptMove(Vector.VECTOR_UP);
+			else if (justPressed == 'd')
+				game.attemptMove(Vector.VECTOR_RIGHT);
+			else if (justPressed == 's')
+				game.attemptMove(Vector.VECTOR_DOWN);
+			else if (justPressed == 'a')
+				game.attemptMove(Vector.VECTOR_LEFT);
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		//do nothing
-		
+		released = true;
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		//do nothing
+		// do nothing
 	}
 
 }
